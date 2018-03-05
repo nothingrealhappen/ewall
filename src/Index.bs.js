30a341ba876175738ca53acd32cf6c4ffabb820a
Yaodong Li <ydli@thoughtworks.com>
2018-03-05 23:19:36 +0800

    Add npm start and and google social login

--- src/Index.bs.js
+++ src/Index.bs.js
@@ -5,6 +5,6 @@ var ReactDOMRe = require("reason-react/src/ReactDOMRe.js");
 var ReasonReact = require("reason-react/src/ReasonReact.js");
 var Page$ReactTemplate = require("./Page.bs.js");
 
-ReactDOMRe.renderToElementWithId(ReasonReact.element(/* None */0, /* None */0, Page$ReactTemplate.make("Hello!", /* array */[])), "index");
+ReactDOMRe.renderToElementWithId(ReasonReact.element(/* None */0, /* None */0, Page$ReactTemplate.make("Hello", /* array */[])), "index");
 
 /*  Not a pure module */
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,21 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const config = require("./config");
+
 const outputDir = path.join(__dirname, "build/");
 
 module.exports = {
-  entry: './src/Index.bs.js',
+  entry: "./src/Index.bs.js",
   output: {
     path: outputDir,
     publicPath: outputDir,
-    filename: 'Index.js',
+    filename: "Index.js"
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "test",
+      template: "src/index.html",
+      googleClientId: config.googleClientId
+    })
+  ]
 };
